f896456ab397e6e59fe25a045438e19bd99dde8e
ClaytonEduard <clayton_eduard@hotmail.com>
2024-02-19 11:02:09 -0300

    First Commit #1

---
+++ src/main/java/br/com/zapcarbackend/zapcar_backend/ZapcarBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.zapcarbackend.zapcar_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapcarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZapcarBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zapcarbackend/zapcar_backend/ZapcarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zapcarbackend.zapcar_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZapcarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
